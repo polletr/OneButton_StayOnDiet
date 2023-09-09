8ce82d1f76973160201fc4b17e1ceb2c331cb652
Daril Camilo <darilvendas@hotmail.com>
2023-09-09 13:49:44 -0500

    Some fixes on Food and Spawner
    We used some references of Leo's code to develop our current mechanics.

--- Assets/Scripts/FoodMovement.cs
+++ Assets/Scripts/FoodMovement.cs
@@ -11,6 +11,9 @@ public class FoodMovement : MonoBehaviour
     public float finalDecline = 0f;
     public bool isCurving = true;
     public int calories = 0;
+    public float lowerBound = -10;
+    [SerializeField]
+    private int foodPoints;
     #endregion
 
     // Start is called before the first frame update
@@ -22,6 +25,11 @@ public class FoodMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y < lowerBound)
+        {
+
+            Destroy(gameObject);
+        }
         #region If statement to determine the movement between the spawner and the player
         if (transform.position.x >= initialDecline)
         {
@@ -42,12 +50,11 @@ public class FoodMovement : MonoBehaviour
         }
         #endregion
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Destroy(targetFood);
-        }
+        CalorieManager.Instance.AddCalorie(calories);
+        ScoreManager.Instance.AddPoints(foodPoints);
+        Destroy(gameObject);
     }
 }
 
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,25 +7,10 @@ public class Spawner : MonoBehaviour
     #region Variables Declaration
     public GameObject[] prefabs = new GameObject[10];
     public float spawnDelay = 2.0f;
-
     #endregion
     // Start is called before the first frame update
     void Start()
     {
-        
-
-        //prefabs[0] = Resources.Load<GameObject>("Assets/Resources/Prefabs/Apple.prefab");
-        //prefabs[1] = Resources.Load<GameObject>("Assets/Resources/Prefabs/Avocado.prefab");
-        //prefabs[2] = Resources.Load<GameObject>("Assets/Resources/Prefabs/Cherry.prefab");
-        //prefabs[3] = Resources.Load<GameObject>("Assets/Resources/Prefabs/PieApple.prefab");
-        //prefabs[4] = Resources.Load<GameObject>("Assets/Resources/Prefabs/EggPlant.prefab");
-        //prefabs[5] = Resources.Load<GameObject>("Assets/Resources/Prefabs/Whiskey.prefab");
-        //prefabs[6] = Resources.Load<GameObject>("Assets/Resources/Prefabs/Sausages.prefab");
-        //prefabs[7] = Resources.Load<GameObject>("Assets/Resources/Prefabs/DragonFruit.prefab");
-        //prefabs[8] = Resources.Load<GameObject>("Assets/Resources/Prefabs/Beer.prefab");
-        //prefabs[9] = Resources.Load<GameObject>("Assets/Resources/Prefabs/Roll.prefab");
-
-        
         //Creation of a execution in loop considering a specific delay
         InvokeRepeating("FoodSpawn", 1, spawnDelay);
     }
@@ -38,6 +23,7 @@ public class Spawner : MonoBehaviour
 
     public void FoodSpawn()
     {
+        //Chose
         int indiceAleatorio = Random.Range(0, prefabs.Length);
         //Instantiate the target food in the spawner original position
         GameObject prefabSelecionado = prefabs[indiceAleatorio];
